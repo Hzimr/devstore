c8ada108db037aa574828b5a2af243522f731531
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-06 17:53:57 -0300

    feat: created context cart

---
+++ src/contexts/cart-context.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { ReactNode, createContext, useContext, useState } from 'react'
+
+interface CartItem {
+  productId: string
+  quantity: number
+}
+
+interface CartContextType {
+  items: CartItem[]
+  addToCart: (productId: string) => void
+}
+
+const CartContext = createContext({} as CartContextType)
+
+export function CartProvider({ children }: { children: ReactNode }) {
+  const [carItems, setCartItems] = useState<CartItem[]>([])
+
+  function addToCart(productId: string) {
+    setCartItems((state) => {
+      const productInCart = state.some((item) => item.productId === productId)
+
+      if (productInCart) {
+        return state.map((item) => {
+          if (item.productId === productId) {
+            return { ...item, quantity: item.quantity + 1 }
+          } else {
+            return item
+          }
+        })
+      } else {
+        return [...state, { productId, quantity: 1 }]
+      }
+    })
+  }
+
+  return (
+    <CartContext.Provider value={{ items: carItems, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => useContext(CartContext)
