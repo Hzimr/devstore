4f79b4ea39b7b2405cd2cb71e53600561667bac6
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-10 16:49:30 -0300

    feat: cy (custom commands)

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -23,7 +23,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
 
@@ -31,6 +31,6 @@ describe('add product to cart', () => {
 
     cy.contains('Adicionar ao carrinho').click()
 
-    cy.contains('Cart(1)').should('exist')
+    cy.contains('Cart (1)').should('exist')
   })
 })
