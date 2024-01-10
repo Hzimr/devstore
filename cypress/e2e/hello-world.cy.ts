f5945a30a87615b843b6708abeccea7cdf3546a6
Hítalo Rodrigues <hitalo005@gmail.com>
2024-01-10 12:59:26 -0300

    feat: install cypress and first test

---
+++ cypress/e2e/hello-world.cy.ts
@@ -0,0 +1,8 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://rocketseat.com.br')
+    cy.contains('Quero assinar agora', {
+      matchCase: false,
+    }).click()
+  })
+})
